af2e4f52495b9404503dbf4c41a0aa8cbb7d5d57
Jyonyve <key_neco@naver.com>
2023-01-16 21:04:14 +0900

    링크수정

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,11 +4,25 @@ import React from "react";
 const Box = (props) => {
   const { title, item, result } = props;
 
+  let index;
+  let value;
+  let itemName;
+  let itemImg;
+
+  if(!!item){
+    index = Object.keys(item)[0];
+    value = item[index];
+    console.log(value);
+    itemName = value.name;
+    itemImg = value.img;
+    console.log(`${itemName} + ${itemImg}`)
+  }
+
   return (
     <div className={`box ${result}`}>
       <h1>{title}</h1>
-      <h2 data-testid="item-name">{item}</h2>
-      <img className="item-img" src={item && item.img} />
+      <h2 data-testid="item-name"> {!!item ? itemName : ''} </h2>
+      <img className="item-img" src={!!item ? itemImg : ''} />
       <h2>{result}</h2>
     </div>
   );
