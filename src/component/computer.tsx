774d8eb543366d22bc16406ff0eed48bfa69250e
Jyonyve <key_neco@naver.com>
2023-01-18 11:22:12 +0900

    해설 주석 추가

--- src/component/computer.tsx
+++ src/component/computer.tsx
@@ -6,22 +6,33 @@ import { choice, rcp } from "../aggregation/choice";
 
 
 function Computer (props:any){
-    const playMap = PlayMap.getPlayMap()!;
+    const playMap = PlayMap.getPlayMap()!; //store기능을 해줄 Map을 import. 
 
     const {gameCount, dataInMap} = props;
 
     const title : string = 'Computer';
     const [comSelect, setComSelect] = useState({});
     const [comResult, setComResult] = useState("");
-    const comShowValue = useRef({});
+    
+    const comShowValue = useRef({}); 
+    //로직 상 컴퓨터의 state가 화면에 표시해야하는 내용보다 빨리 바뀌기 때문에, 바로 이전 승부결과를 담아줄 레퍼런스를 만듦
+    //state의 경우 값이 변경되면 렌더링(화면새로고침)이 이루어지기 때문에, 새로고침을 막기 위해 useRef를 사용한 것. => useRef의 값은 변경되어도 렌더링이 새로 되지 않는다.
+    //만약 comSelect state를 그대로 view단인 Box에 내려보내면, 컴퓨터가 늘 미리 골라두는 다음 판의 본인 패를 화면에 표시하게 되어버림. (나 다음에 가위 낼거다~ 됨)
 
     useEffect(()=> {
         comShowValue.current = comSelect;
-    }, [comSelect])
+    }, [comSelect]) //컴퓨터의 state가 바뀔 때, 레퍼런스 내용을 새 state로 덮어씌워라
     
 
     const randomChoice = () => { //컴퓨터가 랜덤하게 가위 바위 보를 고르는 방법
         const rcpArr : string[] = Object.keys(rcp).filter( value => isNaN(Number(value)));
+        /*  바로 위 코드해설
+            1. string배열 타입의 변수 rcpArr을 선언한다
+            2. Object.keys(rcp) : rcp(Enum)을 배열로 변환하여, 그 배열에서 key를 추출해 새 배열을 만든다 => { rock, 0, scissors, 1, paper, 2}
+            3. Enum은 문자열로 만들어도 자동으로 고유번호 상수가 부여되기 때문에 배열이 저렇게 나오게 됨. 
+            4. 문자와 숫자가 섞인 배열에 .filter를 사용해 필터링된 새 배열을 반환, rcpArr에 대입할 것
+            5. .filter( value => isNaN(Number(value)) : 필터링해서 고를 거야. 내가 가진 어떤 값(value)가, 숫자 형태가 아닌 것만 모아서.
+        */
         let randomIndex : number = Math.floor(Math.random() * rcpArr.length);
         let comRcp = choice(randomIndex);
         return  comRcp;
@@ -33,7 +44,7 @@ function Computer (props:any){
         let comRcp = randomChoice();
         let insertCom : {} = {[comRcp.rcp_choice] : comRcp.rcp_value};
     console.log(`2: com -  comSelect to Map`)             
-        playMap!.set('comSelect', insertCom);
+        playMap!.set('comSelect', insertCom); //ts의 Map은 put대신 set을 씀
     console.log(`3: com -  setComSelect`)
         setComSelect(insertCom);
     }
@@ -58,6 +69,7 @@ function Computer (props:any){
         <>
         <div>
             <Box title={title} item={comShowValue.current} result={comResult} />
+            {/* 위의 렌더링 이슈로 인해, 지금 이순간 컴퓨터의 선택값인 comSelect 대신 comSelect의 상태를 캡쳐하기만 하는 comShowValue.current를 view단으로 내려보냄.*/}
         </div>
         </>
     );
--- src/store/PlayMap.tsx
+++ src/store/PlayMap.tsx
@@ -2,6 +2,7 @@ export default class PlayMap {
 
     //User와 Computer는 동등한 레벨의 형제 객체라 서로 state를 사용하여 통신할 수 없다.
     //그래서 둘 각자가 무엇을 냈는지 담을 맵 스토어를 만들어, 싱글톤을 이용해 공유.
+    //예시를 위해 이 패키치에 js, ts, 클래스형, 함수형을 전부 섞어 쓴 관계로 따로 정통 store(inject나 useStores를 쓰는)로는 만들지않음.
 
     static myInstance :  Map<string,{}>|null = null;
     isDataIn : boolean = false;
